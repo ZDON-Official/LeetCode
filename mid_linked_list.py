069e8e8dd14e2a17548278b4482d250693e04c0b
Zohaib <zrana1214@gmail.com>
2022-07-28 18:37:25 -0400

    slow/fast algo

--- mid_linked_list.py
+++ mid_linked_list.py
@@ -1,7 +1,14 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def middleNode(self, head):
+        slow, fast = head, head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        return slow
