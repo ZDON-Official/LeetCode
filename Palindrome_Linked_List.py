2057c2e916c39036cfedd20e808b3a24fe3b9c06
Zohaib <zrana1214@gmail.com>
2022-07-28 02:33:23 -0400

    created a listNode

--- Palindrome_Linked_List.py
+++ Palindrome_Linked_List.py
@@ -1,8 +1,27 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        #print(head.getVal())
+        Head = head
+        
+        
         return False
+
+
+sol = Solution()
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(2)
+head.next.next.next = ListNode(1)
+
+
+print(sol.isPalindrome(head)) # true
