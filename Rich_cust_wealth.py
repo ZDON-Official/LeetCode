251abd0c7698619e963b7a1e2251f81e1db1d1ed
Zohaib <zrana1214@gmail.com>
2022-07-30 15:02:08 -0400

    finished

--- Rich_cust_wealth.py
+++ Rich_cust_wealth.py
@@ -1,3 +1,17 @@
 class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        return 0
+    def maximumWealth(self, accounts: list[list[int]]) -> int:
+        max_Wealth = 0
+        for i in accounts:
+            tot_Wealth = sum(i)
+            max_Wealth = max(max_Wealth, tot_Wealth)
+
+        return max_Wealth
+
+
+
+sol = Solution()
+
+accounts = [[1,2,3],[3,2,1]]
+p = sol.maximumWealth(accounts)
+
+print(p)
