60303b0ba4ff859cc2cfd769d1e4f629ceacbde9
Zohaib <zrana1214@gmail.com>
2022-07-30 19:58:24 -0400

    finished

--- Rich_wealth.java
+++ Rich_wealth.java
@@ -1,13 +1,21 @@
-public static void main(String[] args) {
+import java.util.stream.IntStream;
+
+public class Rich_wealth {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[][] accounts = {{1,2,3},{3,2,1}};
+
+        System.out.println(sol.maximumWealth(accounts));
+    }
     
 }
-
 
 class Solution {
     public int maximumWealth(int[][] accounts) {
         int max_wealth = 0;
-        
-        for(var i : accounts){
+
+        for (var i : accounts) {
             max_wealth = Math.max(max_wealth, IntStream.of(i).sum());
         }
         return max_wealth;
