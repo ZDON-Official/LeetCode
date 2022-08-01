b08d0c221a84cb630b9842ec37fa29d2e3e6e2fd
Zohaib <zrana1214@gmail.com>
2022-07-31 21:05:32 -0400

    finished - not the best sol

--- LinkedList/add_two_num.py
+++ LinkedList/add_two_num.py
@@ -5,4 +5,27 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        dummy = ListNode(0)
+        curr = dummy
+        
+        l1_s = ""
+        l2_s = ""
+        
+        while l1:
+            l1_s += str(l1.val)
+            l1 = l1.next
+        while l2:
+            l2_s += str(l2.val)
+            l2 = l2.next
+        
+        l1_s = int(l1_s[::-1])
+        l2_s = int(l2_s[::-1])
+            
+        ans = str(l1_s + l2_s)[::-1]
+        
+        for c in ans:
+            newNode = ListNode(int(c))
+            curr.next = newNode
+            curr = newNode
+            
+        return dummy.next
