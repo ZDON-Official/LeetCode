47d74d35286cef4b6b6902592574ae7242c9418c
Zohaib <zrana1214@gmail.com>
2022-08-01 21:07:03 -0400

    Faster Sol using dict

--- LinkedList/Max_Twin_Sum.py
+++ LinkedList/Max_Twin_Sum.py
@@ -1,11 +1,11 @@
+# Maximum Twin Sum of a Linked List
+# https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/
+
 from typing import Optional
-
-
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
 
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
@@ -16,25 +16,19 @@ class Solution:
             n += 1
             tmp = tmp.next
         
-        curr = head
-        i = 0
-        j = 0
-        twinNode = 0
-
-        twinSum = {}
-
-        while curr.next:
-            curr2 = curr.next
-            twinNode = (n-1-i)
-            j = i+1
-            
-            while curr2:
-                if i <= (n/2)-1 and j == twinNode:
-                    currSum = curr.val + curr2.val    
-                j += 1
-                curr2 = curr2.next
-            maxSum = max(maxSum, currSum)
-            i += 1
-            curr = curr.next
+        TwinSum = {}
         
+        curr = head
+        index = 0
+        currSum = 0
+        while curr:
+            if index in TwinSum:
+                currSum = curr.val + TwinSum[index]
+            else:
+                TwinSum[n-1-index] = curr.val
+            
+            maxSum = max(maxSum, currSum)
+            index += 1
+            curr = curr.next
+    
         return maxSum
