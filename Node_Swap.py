db119237eba13f3869fcaa37e05afe99a2ee9b0a
Zohaib <zrana1214@gmail.com>
2022-08-03 18:35:13 -0400

    clean up

--- Node_Swap.py
+++ Node_Swap.py
@@ -1,9 +1,11 @@
-# Definition for singly-linked list.
+# Swapping Nodes in a Linked List
+# https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+from typing import Optional
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
         
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
